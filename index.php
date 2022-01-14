21292cf3ff1a532a9edba0033c2f90b11a633cd8
Bogdan Melkov <hellomelkov@gmail.com>
2022-01-14 18:25:11 +0300

    test2

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
+<?php require_once 'vendor/autoload.php';?>
 <link rel="stylesheet" href="style.css">
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
 <script src="common.js"></script>
-<div class="guss" >
-░░░░░░░░░░░░░░░░░░░░
-░░░░░░░░░░░░░░░░░░░░
-░░░░░░▄▀▀▀▄░░░░░░░░░░
-▄███▀░◐░░░▌░░░░░░░░░
-░░░░▌░░░░░▐░░░░░░░░░
-░░░░▐░░░░░▐░░░░░░░░░
-░░░░▌░░░░░▐▄▄░░░░░░░
-░░░░▌░░░░▄▀▒▒▀▀▀▀▄
-░░░▐░░░░▐▒▒▒▒▒▒▒▒▀▀▄
-░░░▐░░░░▐▄▒▒▒▒▒▒▒▒▒▒▀▄
-░░░░▀▄░░░░▀▄▒▒▒▒▒▒▒▒▒▒▀▄
-░░░░░░▀▄▄▄▄▄█▄▄▄▄▄▄▄▄▄▄▄▀▄
-░░░░░░░░░░░▌▌░▌▌░░░░░
-░░░░░░░░░░░▌▌░▌▌░░░░░
-░░░░░░░░░▄▄▌▌▄▌▌░░░░░
-    <span class="pribyl" >гусь прибыл</span>
+
+
+<button class="call_guss">Зовите гуся</button>
+<div class="body_of_call">
+
 </div>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9928228fb7f3715e9d1041c15ce82f11a2539c93',
+        'name' => 'fleksoy/example',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fleksoy/example' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9928228fb7f3715e9d1041c15ce82f11a2539c93',
+            'dev_requirement' => false,
+        ),
+    ),
+);
